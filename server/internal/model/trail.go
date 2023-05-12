33e87c858b5415ec8806162b528ca9b0f73334ec
tact1m4n3 <tudor.andrei.dicu@gmail.com>
2023-05-12 15:42:45 +0300

    api models modifications

--- server/internal/model/trail.go
+++ server/internal/model/trail.go
@@ -3,9 +3,9 @@ package model
 type Trail struct {
 	Id     int      `json:"id" gorm:"primarykey"`
 	Name   string   `json:"name"`
-	Start  string   `json:"start"`
-	End    string   `json:"end"`
-	Length float64  `json:"length" gorm:"type:decimal(10,6)"`
+	From   string   `json:"from"`
+	To     string   `json:"to"`
+	Length float64  `json:"length" gorm:"type:decimal(8,6)"`
 	Symbol string   `json:"symbol"`
 	Points []*Point `json:"-"`
 }
@@ -13,6 +13,6 @@ type Trail struct {
 type Point struct {
 	Id      int     `json:"-" gorm:"primarykey"`
 	TrailId int     `json:"-"`
-	Lat     float64 `json:"lat" gorm:"type:decimal(10,6)"`
-	Lon     float64 `json:"lon" gorm:"type:decimal(10,6)"`
+	Lat     float64 `json:"lat" gorm:"type:decimal(10,8)"`
+	Lon     float64 `json:"lon" gorm:"type:decimal(11,8)"`
 }
